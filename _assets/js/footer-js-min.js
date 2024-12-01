2fe8541756b455cc22328fd1eb7151b848774218
webb-wanderer <140278726+webb-wanderer@users.noreply.github.com>
2024-12-01 10:09:36 -0800

    many changes and updates

---
+++ _assets/js/footer-js-min.js
@@ -0,0 +1,1 @@
+$("#nav-jh").load("toc.htm"),$("#nav-hhp").load("/_assets/includes/nav-hhp.htm"),$("#nav-jh").load("/_assets/includes/nav-jh.htm"),$("#footer").load("/_assets/includes/footer.htm");
--- _assets/js/footer-js.js
+++ _assets/js/footer-js.js
@@ -1,3 +1,4 @@
 $( "#nav-jh" ).load( "toc.htm" );
 $( "#nav-hhp" ).load( "/_assets/includes/nav-hhp.htm" );
+$( "#nav-jh" ).load( "/_assets/includes/nav-jh.htm" );
 $( "#footer" ).load( "/_assets/includes/footer.htm" );
